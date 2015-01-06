a43124b61ce8a327435dbc78f4f7c22bb81c12be
sirajrkhan <sirajrkhan@live.in>
2015-01-06 05:37:01 +0000

    basic version

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rwdAnalyticsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rwdAnalyticsApp
+ */
+angular.module('rwdAnalyticsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rwdAnalyticsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rwdAnalyticsApp
+ */
+angular.module('rwdAnalyticsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
