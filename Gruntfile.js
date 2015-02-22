572b0e890574a0420cc971e19d5bc0d4b45b9321
Siraj Khan <sirajrkhan@live.in>
2015-02-22 21:10:34 +0530

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 // Generated on 2015-01-05 using generator-angular 0.9.2
 'use strict';
 
-// # Globbing
+// # Globbing…
 // for performance reasons we're only matching one level down:
 // 'test/spec/{,*/}*.js'
 // use this if you want to recursively match all subfolders:
